a177447f8ac6848076942bc27b9db94f13cd63c4
a.studenic@gmail.com <a.studenic@gmail.com@6e87ea4b-b36c-b406-4afb-a7060aeef466>
2013-05-02 21:33:42 +0000

    first commit of IngredientDAOImpl and RecipeDAOImpl. CRUD methods are done, NOT TESTED

--- src/main/java/cz/muni/fi/pb138/evidenceskladurestaurace/persistence/IngredientDAOImpl.java
+++ src/main/java/cz/muni/fi/pb138/evidenceskladurestaurace/persistence/IngredientDAOImpl.java
@@ -1,36 +1,153 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pb138.evidenceskladurestaurace.persistence;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import sun.awt.UngrabEvent;
 
 /**
  *
- * @author Filip Bogyai
+ * @author Adam Studenic
  */
 public class IngredientDAOImpl implements IngredientDAO {
 
+    private Document doc;
+
+    @Override
     public void create(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Element ingr = doc.createElement("ingredient");
+        ingr.setTextContent(ingredient.getName());
+
+        Element unit = doc.createElement("unit");
+        unit.setTextContent(ingredient.getUnit().toString());
+
+        Element amount = doc.createElement("amount");
+        amount.setTextContent(String.valueOf(ingredient.getAmount()));
+
+        Element product = doc.createElement("product");
+        product.appendChild(ingr);
+        product.appendChild(unit);
+        product.appendChild(amount);
+
+        Element products = doc.getDocumentElement();
+        products.appendChild(product);
+
     }
 
+    //should return null?
+    @Override
     public Ingredient findIngredientsByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Unit unit = null;
+        int amount = 0;
+
+        Element product = getProductElementByName(name);
+        NodeList productItems = product.getChildNodes();
+        for (int i = 0; i < productItems.getLength(); i++) {
+            Element item = null;
+            if (productItems.item(i) instanceof Element) {
+                item = (Element) productItems.item(i);
+            }
+
+            if (item.getNodeName().equals("unit")) {
+                String unitName = item.getTextContent();
+                switch (unitName) {
+                    case "g":
+                        unit = Unit.g;
+                        break;
+                    case "kg":
+                        unit = Unit.kg;
+                        break;
+                    case "ml":
+                        unit = Unit.ml;
+                        break;
+                    case "l":
+                        unit = Unit.l;
+                        break;
+                    case "pcs":
+                        unit = Unit.pcs;
+                        break;
+                    default:
+                        unit = Unit.g;
+                        break;
+                }
+            }
+
+            if (item.getNodeName().equals("amount")) {
+                amount = Integer.parseInt(item.getTextContent());
+            }
+        }
+
+        return new Ingredient(name, unit, amount);
+
     }
 
+    @Override
     public void update(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //TODO update name?unit?
+        Element product = getProductElementByName(ingredient.getName());
+        NodeList productItems = product.getChildNodes();
+
+        for (int i = 0; i < productItems.getLength(); i++) {
+            Element item = null;
+            if (productItems.item(i) instanceof Element) {
+                item = (Element) productItems.item(i);
+            }
+
+            if (item.getNodeName().equals("amount")) {
+                item.setNodeValue(String.valueOf(ingredient.getAmount())); //should += add or just replace
+            }
+        }
+        /*
+         write to DOM?
+         */
     }
 
+    @Override
     public void delete(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Element product = getProductElementByName(ingredient.getName());
+        doc.getDocumentElement().removeChild(product);
     }
 
+    @Override
     public List<Ingredient> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Ingredient> ingredientList = new ArrayList<>();
+        NodeList nameList = doc.getElementsByTagName("ingredient");
+
+        for (int i = 0; i < nameList.getLength(); i++) {
+            Element item = null;
+            if (nameList.item(i) instanceof Element) {
+                item = (Element) nameList.item(i);
+            }
+            Ingredient ingredient = findIngredientsByName(item.getTextContent());
+            ingredientList.add(ingredient);
+        }
+
+        return ingredientList;
     }
-  
-    
+
+    public Element getProductElementByName(String name) {
+
+        Element el = null;
+        NodeList productList = doc.getElementsByTagName("product");
+
+        for (int i = 0; i < productList.getLength(); i++) {
+            if (productList.item(i) instanceof Element) {
+                NodeList productItems = productList.item(i).getChildNodes();
+
+                for (int j = 0; j < productItems.getLength(); j++) {
+                    if (productItems.item(j) instanceof Element) {
+                        Element item = (Element) productItems.item(j);
+
+                        if (item.getTextContent().equals(name)) {
+                            el = (Element) productList.item(i);
+                        }
+                    }
+                }
+            }
+        }
+        return el;
+    }
 }
--- src/main/java/cz/muni/fi/pb138/evidenceskladurestaurace/persistence/RecipeDAOImpl.java
+++ src/main/java/cz/muni/fi/pb138/evidenceskladurestaurace/persistence/RecipeDAOImpl.java
@@ -1,36 +1,191 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pb138.evidenceskladurestaurace.persistence;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  *
- * @author Filip Bogyai
+ * @author Adam Studenic
  */
 public class RecipeDAOImpl implements RecipeDAO {
 
+    private Document doc;
+
+    @Override
     public void create(Recipe recipe) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        List<Ingredient> ingredientList = recipe.getIngredients();
+
+        Element parts = doc.createElement("parts");
+
+        for (Ingredient ingredient : ingredientList) {
+            Element ingr = doc.createElement("ingredient");
+            ingr.setTextContent(ingredient.getName());
+
+            Element unit = doc.createElement("unit");
+            unit.setTextContent(ingredient.getUnit().toString());
+
+            Element amount = doc.createElement("amount");
+            amount.setTextContent(String.valueOf(ingredient.getAmount()));
+
+            Element part = doc.createElement("part");
+            part.appendChild(ingr);
+            part.appendChild(unit);
+            part.appendChild(amount);
+
+            parts.appendChild(part);
+        }
+
+        Element name = doc.createElement("name");
+        name.setTextContent(recipe.getName());
+
+        Element receipt = doc.createElement("receipt");
+        receipt.appendChild(name);
+        receipt.appendChild(parts);
+
+        Element receipts = doc.getDocumentElement();
+        receipts.appendChild(receipt);
+
     }
 
+    @Override
     public Recipe findRecipesByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Ingredient> ingredientList = null;
+
+        Element receipt = getReceiptElementByName(name);
+        ingredientList = findAllIngredientsInReceiptElement(receipt);
+
+        return new Recipe(name, ingredientList);
+
     }
 
+    @Override
     public void update(Recipe recipe) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Element receipt = getReceiptElementByName(recipe.getName());
+        NodeList parts = receipt.getElementsByTagName("part");
+        List<Ingredient> ingredientList = recipe.getIngredients();
+
+        for (int i = 0; i < parts.getLength(); i++) {
+            Element part = null;
+            if (parts.item(i) instanceof Element) {
+                part = (Element) parts.item(i);
+            }
+
+            //parts have to be in the same order, refactor & test
+            NodeList partItems = part.getChildNodes();
+            for (int j = 0; j < partItems.getLength(); j++) {
+                Element item = null;
+                if (parts.item(i) instanceof Element) {
+                    item = (Element) parts.item(i);
+                }
+                if (item.getNodeName().equals("amount")) {
+                    item.setNodeValue(String.valueOf(ingredientList.get(i).getAmount())); //should += add or just replace !!ORDER
+                }
+            }
+        }
     }
 
+    @Override
     public void delete(Recipe recipe) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Element receipt = getReceiptElementByName(recipe.getName());
+        doc.getDocumentElement().removeChild(receipt);
     }
 
+    @Override
     public List<Recipe> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Recipe> receiptList = new ArrayList<>();
+        NodeList receiptNameList = doc.getElementsByTagName("name");
+
+        for (int i = 0; i < receiptNameList.getLength(); i++) {
+            Element item = null;
+            if (receiptNameList.item(i) instanceof Element) {
+                item = (Element) receiptNameList.item(i);
+            }
+
+            Recipe receipt = findRecipesByName(item.getTextContent());
+            receiptList.add(receipt);
+        }
+
+        return receiptList;
     }
-    
-    
+
+    public Element getReceiptElementByName(String name) {
+
+        Element el = null;
+        NodeList receiptList = doc.getElementsByTagName("receipt");
+
+        for (int i = 0; i < receiptList.getLength(); i++) {
+            if (receiptList.item(i) instanceof Element) {
+                Element nameElement = (Element) receiptList.item(i).getFirstChild();
+
+                if (nameElement.getTextContent().equals(name)) {
+                    el = (Element) receiptList.item(i);
+                }
+            }
+        }
+        return el;
+    }
+
+    public List<Ingredient> findAllIngredientsInReceiptElement(Element receipt) {
+        List<Ingredient> ingredientList = new ArrayList<>();
+
+        NodeList parts = receipt.getElementsByTagName("part");
+
+        for (int i = 0; i < parts.getLength(); i++) {
+            Element part = null;
+            if (parts.item(i) instanceof Element) {
+                part = (Element) parts.item(i);
+            }
+
+            String ingredientName = null;
+            Unit unit = null;
+            int amount = 0;
+
+            NodeList partItems = part.getChildNodes();
+            for (int j = 0; j < partItems.getLength(); j++) {
+                Element item = null;
+                if (partItems.item(j) instanceof Element) {
+                    item = (Element) partItems.item(i);
+                }
+
+                if (item.getNodeName().equals("ingredient")) {
+                    ingredientName = item.getTextContent();
+                }
+
+                if (item.getNodeName().equals("unit")) {
+                    String unitName = item.getTextContent();
+                    switch (unitName) {
+                        case "g":
+                            unit = Unit.g;
+                            break;
+                        case "kg":
+                            unit = Unit.kg;
+                            break;
+                        case "ml":
+                            unit = Unit.ml;
+                            break;
+                        case "l":
+                            unit = Unit.l;
+                            break;
+                        case "pcs":
+                            unit = Unit.pcs;
+                            break;
+                        default:
+                            unit = Unit.g;
+                            break;
+                    }
+                }
+
+                if (item.getNodeName().equals("amount")) {
+                    amount = Integer.parseInt(item.getTextContent());
+                }
+            }
+            ingredientList.add(new Ingredient(ingredientName, unit, amount));
+        }
+
+        return ingredientList;
+    }
 }
